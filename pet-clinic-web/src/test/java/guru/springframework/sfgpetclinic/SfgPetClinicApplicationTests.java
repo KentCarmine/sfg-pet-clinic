751cc90030aaf306d591f81d4fda4c841cd92109
Kent Carmine <5168853+KentCarmine@users.noreply.github.com>
2019-09-06 00:45:23 -0500

    Updated to junit 5. Closes #4

---
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,15 @@
+package guru.springframework.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+//@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
+public class SfgPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
